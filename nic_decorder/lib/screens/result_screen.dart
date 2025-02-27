624de716f5526a9f7bf99d804151837d8fef51f0
Pubudu Sanka <pubudusanka79@gmail.com>
2025-02-27 20:05:43 +0530

    Update result_screen.dart

--- nic_decorder/lib/screens/result_screen.dart
+++ nic_decorder/lib/screens/result_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../controllers/nic_controller.dart';
 
 class ResultScreen extends StatelessWidget {
   final NicController nicLogic = Get.find();
