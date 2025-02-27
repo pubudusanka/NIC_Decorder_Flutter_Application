20d811919261cb9c1a1a88297080513f65640633
Pubudu Sanka <pubudusanka79@gmail.com>
2025-02-27 19:49:35 +0530

    create flutter project

---
+++ nic_decorder/README.md
@@ -0,0 +1,3 @@
+# nic_decorder
+
+A new Flutter project.
---
+++ nic_decorder/android/app/src/main/kotlin/com/example/nic_decorder/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nic_decorder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
